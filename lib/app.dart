2d993c26d257e7264e652136938dce49b3979858
Ri-2020 <rohitgupta111abcd@gmail.com>
2022-09-13 13:34:54 +0530

    The starting work

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            children: const [
+              Text(
+                'Product List',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/data.dart
@@ -0,0 +1,37 @@
+const productData = [
+  {
+    "title": "OPEE Product 1",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 1. It is a good product to buy",
+  },
+  {
+    "title": "OPEE Product 2",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 2. It is a good product to buy",
+  },
+  {
+    "title": "OPEE Product 3",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 3. It is a good product to buy",
+  },
+  {
+    "title": "OPEE Product 4",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 4. It is a good product to buy",
+  },
+  {
+    "title": "OPEE Product 5",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 5. It is a good product to buy",
+  },
+  {
+    "title": "OPEE Product 6",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 6. It is a good product to buy",
+  },
+  {
+    "title": "OPEE Product 7",
+    "imageUrl": "https://picsum.photos/200/300",
+    "description": " This is product 7. It is a good product to buy",
+  },
+];
